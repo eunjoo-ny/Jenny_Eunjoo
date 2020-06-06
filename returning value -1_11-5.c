c34c2c7901079159215016c963fa98e684657918
Eunjoo ahn <66054128+eunjoo-ny@users.noreply.github.com>
2020-06-06 20:00:05 +0900

    Add files via upload

---
+++ returning value -1_11-5.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(void) {
+	int res;
+	char ch;
+	while(1){//while반복문임.
+	scanf("%c", &ch);
+	if (res == -1) 
+		break;
+
+		printf("%d", ch);
+	}
+	return 0;
+}
